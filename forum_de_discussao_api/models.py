18e8e783380ed94a29b0cc666268209e96582e2f
Iago Ramone <iagoramone.fisica@gmail.com>
2024-10-05 20:14:22 -0300

    Update models.py

--- forum_de_discussao_api/models.py
+++ forum_de_discussao_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Modelo de banco de dados para os usuários do sistema"""
+    email = models.EmailField(max_lenght=255, unique=True)
+    name = models.CharField(max_lenght=255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['name']
+
+     def get_full_name(self):
+         """Pega o nome completo da pessoa """
+        return self.email
+
+    def get_short_name(self):
+        """Pega o menor nome """
+        return self.name
